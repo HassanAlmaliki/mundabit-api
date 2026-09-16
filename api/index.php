c091429432aee484740f40c6f8f81e2f492c0e44
HassanAlmaliki <almalikihasan77@gmail.com>
2026-09-17 00:38:02 +0300

    Debug Vercel env and catch errors

--- api/index.php
+++ api/index.php
@@ -1,7 +1,9 @@
 <?php
-echo "API INDEX EXECUTED<br>";
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+echo "VERCEL ENV: " . (isset($_ENV['VERCEL']) ? 'YES' : 'NO') . "<br>";
+echo "VERCEL SERVER: " . (isset($_SERVER['VERCEL']) ? 'YES' : 'NO') . "<br>";
+
 
 // Prepare storage directories in /tmp for Vercel serverless environment
 if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
@@ -27,5 +29,9 @@ if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
     putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 }
 
-// Forward execution to Laravel entrypoint
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>FATAL ERROR CAUGHT IN API/INDEX.PHP</h1>";
+    echo "<pre>" . (string) $e . "</pre>";
+}
